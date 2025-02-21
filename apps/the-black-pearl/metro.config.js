06702026f693ab1854cf520b9f5c9c09a5123f2e
bonfaceZane <bonfacezane@gmail.com>
2025-02-21 15:41:36 +0100

    prettier: fix all command

--- apps/the-black-pearl/metro.config.js
+++ apps/the-black-pearl/metro.config.js
@@ -1,5 +1,5 @@
-const { withNxMetro } = require('@nx/react-native');
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNxMetro } = require("@nx/react-native");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -11,22 +11,22 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 const customConfig = {
-  cacheVersion: 'the-black-pearl',
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
-  },
+    cacheVersion: "the-black-pearl",
+    transformer: {
+        babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    },
+    resolver: {
+        assetExts: assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...sourceExts, "cjs", "mjs", "svg"],
+    },
 };
 
 module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
-  debug: false,
-  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
-  extensions: [],
-  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-  watchFolders: [],
+    // Change this to true to see debugging info.
+    // Useful if you have issues resolving modules
+    debug: false,
+    // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+    extensions: [],
+    // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+    watchFolders: [],
 });
