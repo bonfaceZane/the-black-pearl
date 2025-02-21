8974ed01e9b4e428123c5494fb9f959487b53cad
bonfaceZane <bonfacezane@gmail.com>
2025-02-21 22:14:06 +0100

    addd vstack and hstack

--- apps/pearl/app/(tabs)/index.tsx
+++ apps/pearl/app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
-import { Image, StyleSheet, Platform } from "react-native";
-
+import { Image, StyleSheet } from "react-native";
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { Checkbox } from "@/components/ui/checkbox";
+import { VStack } from "@/components/ui/vstack";
 
 export default function Home() {
     return (
@@ -21,48 +20,9 @@ export default function Home() {
                 <ThemedText type="title">Welcome! to home</ThemedText>
                 <HelloWave />
             </ThemedView>
-            <Checkbox value={"test"} />
-            <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-                <ThemedText>
-                    Edit{" "}
-                    <ThemedText type="defaultSemiBold">
-                        app/(tabs)/index.tsx
-                    </ThemedText>{" "}
-                    to see changes. Press{" "}
-                    <ThemedText type="defaultSemiBold">
-                        {Platform.select({
-                            ios: "cmd + d",
-                            android: "cmd + m",
-                            web: "F12",
-                        })}
-                    </ThemedText>{" "}
-                    to open developer tools.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-                <ThemedText>
-                    Tap the Explore tab to learn more about what's included in
-                    this starter app.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">
-                    Step 3: Get a fresh start
-                </ThemedText>
-                <ThemedText>
-                    When you're ready, run{" "}
-                    <ThemedText type="defaultSemiBold">
-                        npm run reset-project
-                    </ThemedText>{" "}
-                    to get a fresh{" "}
-                    <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-                    directory. This will move the current{" "}
-                    <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-                    <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-                </ThemedText>
-            </ThemedView>
+            <VStack>
+                <ThemedText type="default">I am a Heading</ThemedText>
+            </VStack>
         </ParallaxScrollView>
     );
 }
