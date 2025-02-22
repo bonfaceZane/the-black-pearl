48dbc52b564458ab8d5c818436f08ac36d2c6e65
bonfaceZane <bonfacezane@gmail.com>
2025-02-22 04:23:41 +0100

    fixed image component

--- apps/pearl/components/card.tsx
+++ apps/pearl/components/card.tsx
@@ -1,10 +1,10 @@
-import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Card as CardComponent } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
-import { Image } from "expo-image";
+import { HStack } from "./ui/hstack";
+import { Image } from "@/components/ui/image";
 
 type Props = {
     title: string;
@@ -37,19 +37,19 @@ export function Card({ title, description, image }: Props) {
                     white and black.
                 </Text>
             </VStack>
-            {/* <Box className="flex-col sm:flex-row">
-                <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+            <HStack className="gap-2">
+                <Button className="w-1/2">
                     <ButtonText size="sm">Add to cart</ButtonText>
                 </Button>
                 <Button
                     variant="outline"
-                    className="px-4 py-2 border-outline-300 sm:flex-1"
+                    className="w-1/2"
                 >
                     <ButtonText size="sm" className="text-typography-600">
                         Wishlist
                     </ButtonText>
                 </Button>
-            </Box> */}
+            </HStack>
         </CardComponent>
     );
 }
