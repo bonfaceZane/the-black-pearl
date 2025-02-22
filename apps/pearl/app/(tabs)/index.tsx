6e00cb775c333f572b0ae626cb794eb0b5049180
bonfaceZane <bonfacezane@gmail.com>
2025-02-22 02:31:05 +0100

    updated text

--- apps/pearl/app/(tabs)/index.tsx
+++ apps/pearl/app/(tabs)/index.tsx
@@ -1,8 +1,5 @@
-import { StyleSheet } from "react-native";
-import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { VStack } from "@/components/ui/vstack";
 import { useTranslation } from "react-i18next";
 import { Image } from "expo-image";
@@ -39,32 +36,9 @@ export default function Home() {
                 </Swiper>
             }
         >
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">{t("title")}</ThemedText>
-                <HelloWave />
-            </ThemedView>
-            <VStack>
+            <VStack className="">
                 <ThemedText type="default">{t("description")}</ThemedText>
             </VStack>
         </ParallaxScrollView>
     );
 }
-
-const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: "absolute",
-    },
-});
